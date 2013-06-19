c1134af5ef9ce3d8dc2f1594c6d2e40dedb8d8c5
Luke Rattei <luker@tableproject.org>
2013-06-19 02:44:18 -0500

    Add formula for Gearman 0.24.

---
+++ gearman.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Gearman < Formula
+  url 'http://launchpad.net/gearmand/1.0/0.24/+download/gearmand-0.24.tar.gz'
+  homepage 'http://gearman.org/'
+  md5 '7cd3bfedd37fd8f2d2c9a59333e08c8c'
+
+  depends_on 'libevent'
+  depends_on 'boost'
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
