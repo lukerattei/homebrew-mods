bc567e8ab76a478a043186404c6891b8f3797d9b
Luke Rattei <luker@tableproject.org>
2013-06-19 03:02:28 -0500

    Rename class name to match formula filename change.

--- gearman024.rb
+++ gearman024.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Gearman < Formula
+class Gearman024 < Formula
   url 'http://launchpad.net/gearmand/1.0/0.24/+download/gearmand-0.24.tar.gz'
   homepage 'http://gearman.org/'
   md5 '7cd3bfedd37fd8f2d2c9a59333e08c8c'
